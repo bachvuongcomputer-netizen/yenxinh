8f9255a8c07567675982f9748ac8466389739fa4
bachvuongcomputer-netizen <bachvuong.computer@gmail.com>
2026-07-08 10:13:56 +0700

    Add functionality to open letter and show alert

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+const intro = document.getElementById("intro");
+const letter = document.getElementById("letter");
 
+document.getElementById("openLetter").onclick = function () {
+
+    intro.style.display = "none";
+    letter.style.display = "block";
+
+};
+
+document.getElementById("loveBtn").onclick = function () {
+
+    alert("❤️ Chúc em Yến mau khỏi bệnh nhé! ❤️");
+
+};
